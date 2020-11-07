e92834b446cfd04f0648a659f84a82d0d1e37e09
Daniel Korchinski <danielkorchinski@gmail.com>
2020-11-07 12:29:25 -0800

    updated skeleton class

--- simple-trace.py
+++ simple-trace.py
@@ -6,6 +6,7 @@ v = np.array([0.6,0,0])
 #todo: compute Lambda 
 
 planeList = []
+Nplanes = len(planeList)
 #todo: plane class: 
 #initialization 
 
@@ -15,5 +16,18 @@ pixelX,pixelY = np.meshgrid(np.arange(0,))
 Nrays = np.size(pixels) #can be more later if we want anti-aliasing
 rays = np.zeros([Nrays,4])
 
-for pl in planeList:
-    tIntersects = intersection(pl,rays)
+
+planeList = np.arange(Nplanes)
+intersectingPlaneIndex = -1*np.ones(Nrays,dtype=np.int32)
+leastT = 1e99 * np.ones(Nrays)
+rayRGB = np.zeros([Nrays,3],dtype = int32)
+for ind,pl in enumerate(planeList):
+    color,tIntersects = intersection(pl,rays)
+    
+    
+    intersectingRayIndices = np.arange(Nrays)[np.and(leastT > tIntersects, tIntersects>0)]
+    intersectingPlaneIndex[intersectingRayIndices] = ind 
+    leastT[intersectingRayIndices] = tIntersects[intersectingRayIndices]
+    rayRGB[intersectingRayIndices,:] = color[intersectingRayIndices]
+
+#TODO: output the ray RGBs into an image.
